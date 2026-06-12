3e1a99f0b7aa78469cf5928a569c166770add0ee
Harakesh92 <Harakeshrajput92@gmail.com>
2026-06-12 12:27:50 +0530

    rename folder

---
+++ DAY01/FectorialOfGivenNumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num, factorial = 1;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    for(int i=1; i<=num; ++i)
+    {
+        factorial=factorial*i; // factorial = factorial * i
+    }
+    printf("Factorial of %d = %d", num, factorial);
+    return 0;
+}
