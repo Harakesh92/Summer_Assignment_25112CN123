93fd5bd4db9e0f18f78ae3d654f45b37b9dd9b3a
Harakesh Rajput <harakeshrajput92@gmail.com>
2026-06-20 16:58:02 +0530

    Add files via upload

---
+++ DAY20/FindRowWiseSum.c
@@ -0,0 +1,37 @@
+// Write a program to Find row-wise sum. 
+
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j, sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Row-wise sum:\n");
+    for(i = 0; i < r; i++)
+    {
+        sum = 0;
+
+        for(j = 0; j < c; j++)
+        {
+            sum = sum + a[i][j];
+        }
+
+        printf("Sum of Row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
